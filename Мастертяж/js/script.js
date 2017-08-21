97b9066e3bcdc9336117c7911992fbf1a276aa3d
jchernyuk <jchernyuk@gmail.com>
2017-08-22 00:53:27 +0300

    исправлен баг слика

---
+++ Мастертяж/js/script.js
@@ -0,0 +1,15 @@
+$('.technique_slider').slick({
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+
+
+
+//Проверка работы jQuery
+//$(document).ready(function(){
+// alert(jQuery.fn.jquery);
+// });
